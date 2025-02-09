d80d70da4d4bf06dbd11aec9efc5bf143425e8fb
Vladonbass <129100818+vladqwwerstfygjuhlj@users.noreply.github.com>
2025-02-09 13:44:14 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-result = []
+import random
+import string
 
-def divider(a, b):
-    try:
-        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-            raise TypeError("Один із аргументів не є числом")
-        if a < b:
-            raise ValueError("Число 'a' менше за 'b'")
-        if b > 100:
-            raise IndexError("Значення 'b' більше 100")
-        return a / b
-    except Exception as e:
-        print(f"Помилка у divider({a}, {b}): {e}")
-        return None 
+def random_letter_generator():
+
+    while True:
+        yield random.choice(string.ascii_letters) 
 
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
-
-for key, value in data.items():
-    try:
-        res = divider(key, value)
-        if res is not None: 
-            result.append(res)
-    except Exception as e:
-        print(f"Помилка обробки ключа {key}: {e}")
-
-print("Результат:", result)
+gen = random_letter_generator()
+for _ in range(10):
+    print(next(gen), end=" ")
 
