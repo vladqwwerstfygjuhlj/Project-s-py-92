bfd7e098a9c5c54418e535cd1544aa180c6e27b6
Vladonbass <129100818+vladqwwerstfygjuhlj@users.noreply.github.com>
2025-02-09 13:30:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+
+        return f"[{self.year}] {self.make} {self.model}"
+car1 = Car("Chevrolet", "Impala", 1967)
+car2 = Car("Buick", "Riviera", 1963)
+
+print(car1.get_info())  # [2020] Toyota Camry
+print(car2.get_info())  # [2022] Tesla Model S
+
